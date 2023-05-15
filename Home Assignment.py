cd016cac0983f3e7fc0923c349a02ccb39419ab1
yashshetty26 <133508684+yashshetty26@users.noreply.github.com>
2023-05-15 16:49:14 +0530

    Add files via upload

---
+++ Home Assignment.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+A1 = "Yash"
+type (A1)
+
+
+# In[4]:
+
+
+A2= [1,2,3,4,5,6,7,]
+type(A2)
+
+
+# In[5]:
+
+
+A3= 26.111111111
+type(A3)
+
+
+# In[6]:
+
+
+A4= ("Yash", [1,2,3,4,5,6,7], 26.1111111)
+type(A4)
+
+
+# In[2]:
+
+
+var1 = ' '
+type(var1)
+
+
+# In[7]:
+
+
+var2 = '[ DS , ML , Python]'
+type(var2)
+
+
+# In[9]:
+
+
+var3 =  ['DS' , 'ML' , 'Python']
+type(var3)
+
+
+# In[10]:
+
+
+var4 = 1
+type(var4)
+
+
+# ## In Python / Stands For:
+# ### The / symbol in python stands for doing the division operation.
+# #### For Example:
+
+# In[12]:
+
+
+24/6
+
+
+# ## In Python % Stands For:
+# ### The % symbol in Python is called the Modulo Operator. It returns the remainder of a division opearation.
+# #### For Example:
+# 
+
+# In[13]:
+
+
+27%6
+
+
+# ## In Python // Stands For:
+# ### The // symbol in Python is called the Floor Division Operator. Floor division is a normal division operation except that it returns the largest possible integer.
+# #### For Example:
+
+# In[14]:
+
+
+26//5
+
+
+# ## In Python ** Stands For:
+# ### The ** symbol in Python is called the Exponential Operator. Exponents is an operator which displays the operation of numerical powers.
+# #### For Example:
+# 
+
+# In[15]:
+
+
+12**2
+
+
+# In[16]:
+
+
+d= ("a", "b", "c", [3,5,7,12], 24.08000000)
+print("Element: str + int + float")
+
+
+# In[ ]:
+
+
+if 24//8 = 0 while 24/8 = 3
+("24 is purely divisible with 8")
+else 
+("24 is not purely divisible with 8")
+
+
+# In[ ]:
+
+
+(3,6,9,12,15,18,21,24,27,30) 
+if (3,6,9,12,15,18,21,24,27,30) is // 3 = 0 
+("it is divisible by 3")
+else
+("it is not divisible by 3")
+
+
+# ## Mutable Objects
+# ### Mutable objects are those that allow you to change their value or data in place without affecting the object's identity. 
+# 
+# ## Immutable Objects
+# ### In contrast, immutable objects don't allow this kind of operation. You'll just have the option of creating new objects of the same type with different values.
+
+# In[ ]:
+
+
+
+
